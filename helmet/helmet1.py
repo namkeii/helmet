f088facd17a9e3e444090776813a12079d7fb52a
namkeii <namanovarma@gmail.com>
2023-11-21 14:29:25 +0530

    Added Virtual Helmets with temp data

---
+++ helmet/helmet1.py
@@ -0,0 +1,18 @@
+import socket
+import time
+import subprocess
+import os
+
+host = '192.168.128.168'
+port = 5000
+
+message = " 10 200 4 l l"
+count = 10;
+while(True):
+    dat = str(count)+message
+    client_socket = socket.socket()
+    client_socket.connect((host,port))
+    client_socket.send(dat.encode())
+    count+=1
+    time.sleep(5)
+    print("Data Sent");
